78c1e8c6877028527a322543de1dd6bb93796bf9
Pravin <pprakash@adobe.com>
2019-07-15 13:54:46 -0700

    test Run 2

--- environmentSetup.sh
+++ environmentSetup.sh
@@ -1,14 +1,19 @@
 
 function copyEnvVarsToGradleProperties {
     echo "Its running......"
-    echo $PATH
-    # GRADLE_PROPERTIES="/Users/pprakash/Places/places-monitor-android/code/gradle.properties"
-    # export GRADLE_PROPERTIES
-    # echo "Gradle Properties should exist at $GRADLE_PROPERTIES"
+    echo $HOME
+    GRADLE_PROPERTIES=$HOME"/code/code/gradle.properties"
+    export GRADLE_PROPERTIES
+    echo "Gradle Properties should exist at $GRADLE_PROPERTIES"
 
-    # if [ ! -f "$GRADLE_PROPERTIES" ]; then
-    #     echo "Gradle Properties does not exist"
-    # fi
+    if [ ! -f "$GRADLE_PROPERTIES" ]; then
+        echo "Gradle Properties does not exist"
+    fi
      echo "Writing TEST_API_KEY to gradle.properties..."
-     echo "TEST_API_KEY=value" >> $GRADLE_PROPERTIES
+     echo "artifactoryUrl=$artifactoryUrl" >> $GRADLE_PROPERTIES
+     echo "artifactoryContextUrl=$artifactoryContextUrl" >> $GRADLE_PROPERTIES
+     echo "artifactorySnapshotUrl=$artifactorySnapshotUrl" >> $GRADLE_PROPERTIES
+     echo "artifactoryRepoKey=$artifactoryRepoKey" >> $GRADLE_PROPERTIES
+     echo "artifactoryUserName=$artifactoryUserName" >> $GRADLE_PROPERTIES
+     echo "artifactoryPassword=$artifactoryPassword" >> $GRADLE_PROPERTIES
 }
